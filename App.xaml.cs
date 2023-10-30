491805a50bd5e9d2969706c3a957f9e43ae64170
Santosh Dahal <exendahal@gmail.com>
2023-10-30 20:26:45 +0545

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MAUI_INIT_PROJECT
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
